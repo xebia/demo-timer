d4223f188f0c6c4605f37d98e674e4f3c46d8f37
Frank van Wijk <fvanwijk@xebia.com>
2021-04-24 17:31:54 +0200

    Set page meta title

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   lintOnSave: false,
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = "Timer";
+      return args;
+    });
+  },
 };
