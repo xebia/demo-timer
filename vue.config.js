93dda29632984bcc752ee9c02869b220947b4dea
Frank van Wijk <fvanwijk@xebia.com>
2021-04-25 05:26:04 +0200

    Improve manifest file

--- vue.config.js
+++ vue.config.js
@@ -6,4 +6,17 @@ module.exports = {
       return args;
     });
   },
+  pwa: {
+    name: "Demo timer",
+    themeColor: "#6c1d5f",
+    msTileColor: "#6c1d5f",
+    manifestOptions: {
+      short_name: "Timer",
+      background_color: "#6c1d5f",
+    },
+    iconPaths: {
+      appleTouchIcon: "img/icons/apple-touch-icon.png",
+      msTileImage: "img/icons/mstile-150x150.png",
+    },
+  },
 };
