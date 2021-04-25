3880d9b08f4b52e5d1d77f0099c2f3fb804b43c3
Frank van Wijk <fvanwijk@xebia.com>
2021-04-25 10:24:42 +0200

    Add Google Analytics

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue';
+import VueGtag from 'vue-gtag-next';
 import App from './App.vue';
 import './registerServiceWorker';
 
-createApp(App).mount('#app');
+createApp(App)
+  .use(VueGtag, {
+    config: {
+      id: 'G-JLMJX3X46F', //
+    },
+  })
+  .mount('#app');
